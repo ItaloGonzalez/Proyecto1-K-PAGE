48d10864ea4b93c1a79a76d8fd0cab5f7b839020
ItaloGonzalez <italogicorv@gmail.com>
2024-12-04 20:42:47 -0300

    Add files via upload

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -32,3 +32,36 @@ document.getElementById('darkModeToggle').addEventListener('click', function() {
         this.textContent = 'Modo Oscuro';
     }
 });
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
+  const query = document.getElementById('searchInput').value.toLowerCase(); // Obtiene el valor de búsqueda
+  const posts = document.querySelectorAll('.tm-post'); // Selecciona todos los artículos
+
+  posts.forEach(post => {
+      const title = post.querySelector('.tm-post-title').textContent.toLowerCase(); // Obtiene el texto del título
+      const content = post.querySelector('p').textContent.toLowerCase(); // Obtiene el texto del párrafo
+
+      // Verifica si el título o el contenido contienen la consulta
+      if (title.includes(query) || content.includes(query)) {
+          post.style.display = ''; // Muestra el post si coincide
+      } else {
+          post.style.display = 'none'; // Oculta el post si no coincide
+      }
+  });
+});
+window.onload = function() {
+  var articles = document.querySelectorAll('.tm-post');
+  var maxHeight = 0;
+
+  // Encuentra la altura máxima
+  articles.forEach(function(article) {
+      if (article.offsetHeight > maxHeight) {
+          maxHeight = article.offsetHeight;
+      }
+  });
+
+  // Aplica la altura máxima a todos los artículos
+  articles.forEach(function(article) {
+      article.style.height = maxHeight + 'px';
+  });
+}
